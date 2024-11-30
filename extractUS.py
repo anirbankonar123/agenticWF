bf65c3f14ba795f7cc729771f58a04f6ee7463cc
TheBuffer <anishkonar10@gmail.com>
2024-11-30 21:31:48 +0530

    adding code

--- extractUS.py
+++ extractUS.py
@@ -11,22 +11,24 @@ print(len(pages))
 text = ""
 for page in pages:
     text+=page.page_content
-
+user_stories=""
 #Prompt to get related requirements for a requirement header
-def make_prompt_Reqmnt_to_US(reqmnt, context):
+def make_prompt_Reqmnt_to_US(reqmnt, context, user_stories):
   escaped = context.replace("'", "").replace('"', "").replace("\n", " ")
-  prompt = ("""You are an expert Domain specialist who can find out relevant Requirements from 
+  prompt = ("""You are an expert Requirement Analyst who can find out relevant Requirements from 
   the Requirement Header given, using text from the context included below. \
   Generate the User stories for all requirement IDs under this Requirement Header. \
+  Do not leave out any Requirement ID for this Requirement.\
   Be sure to include the User Story, Requirement ID, the Description with flows, Acceptance criteria in detail for each Requirement ID. \
-  Map each requirement ID to one User Story strictly, do not combine Requirement IDs into one User Story.\
   Return the information in JSON structure with an array of User stories.\
-  give the output in a valid json format.\
+  Ignore the already available User stories, and do not repeat them.\
+  Generate the output in a strictly valid json format.\
   Requirement header : '{reqmnt}'
+  User stories already available :{user_stories}
   Context: '{context}'
 
   ANSWER:
-  """).format(reqmnt=reqmnt,context=escaped)
+  """).format(reqmnt=reqmnt,context=escaped,user_stories=user_stories)
 
   return prompt
 
@@ -34,19 +36,29 @@ genai.configure()
 model = genai.GenerativeModel('gemini-1.5-pro')
 context = text
 
-temperature=1.5
+temperature=0
 top_p=1.0
 
 reqmnt="Search"
-prompt = make_prompt_Reqmnt_to_US(reqmnt, context)
-response = model.generate_content(prompt,
-    generation_config=genai.types.GenerationConfig(
-        temperature=float(temperature),
-        top_p=float(top_p))
-)
+user_stories_list=[]
 
-print(response.text)
+for i in range (1,3):
+    print(str(i))
+    prompt = make_prompt_Reqmnt_to_US(reqmnt, context, user_stories)
+    response = model.generate_content(prompt,
+        generation_config=genai.types.GenerationConfig(
+            temperature=float(temperature),
+            top_p=float(top_p))
+    )
+    user_stories = response.text[7:-4]
+    print(user_stories)
+    print("**************************************")
+    user_stories_list.append(user_stories)
 
-file1 = open("MyFile.txt", "w")
-file1.write(response.text)
+print(user_stories_list)
+
+file1 = open("US.txt", "w")
+file1.write("".join(user_stories_list))
 file1.close()
+
+#https://cloud.google.com/vertex-ai/generative-ai/docs/learn/models#gemini-1.5-pro
